cfae61db926749ab6f015dc6d2e0a9d847046195
unknown <abilp444@gmail.com>
2023-10-19 09:22:27 +0700

    Tambahan

--- app/src/main/java/com/example/jalanin_aja/DaftarProvinsiActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.jalanin_aja;
-
-import android.app.Activity;
-
-public class DaftarProvinsiActivity extends Activity {
-}
---
+++ app/src/main/java/com/example/jalanin_aja/WisataAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.macroproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class WisataAdapter(private val wisataList:ArrayList<Wisata>)
+    : RecyclerView.Adapter<WisataAdapter.WisataViewHolder>() {
+
+    var onItemClick : ((Wisata) -> Unit)? = null
+    class WisataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val iv_acehdarussalam : ImageView = itemView.findViewById(R.id.iv_acehdarusallam)
+        val tv_aceh : TextView = itemView.findViewById(R.id.tv_aceh)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WisataViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.imageitem, parent, false)
+        return WisataViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WisataViewHolder, position: Int) {
+        val wisata = wisataList[position]
+        holder.iv_acehdarussalam.setImageResource(wisata.image)
+        holder.tv_aceh.text = wisata.name
+
+        holder.itemView.setOnClickListener {
+            val OnItemClick = null
+            OnItemClick?.invoke(wisata)
+        }
+    }
+    override fun getItemCount(): Int {
+        return wisataList.size
+    }
+
+}
